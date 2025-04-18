3a6d489f61fcaddf110788904b543df8a6e656b9
Aashish Jaini <aashish17405@gmail.com>
2025-04-18 19:30:06 +0530

    .

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  }
+
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  }
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-800 text-white">
+      <button onClick={decrement} className="border border-white rounded-full p-3 cursor-pointer">Decrement</button>
+      <div className="p-3">{count}</div>
+      <button onClick={increment} className="border border-white rounded-full p-3 cursor-pointer">Increment</button>
+    </div>
+  );
+}
